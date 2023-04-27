9bb74afa51cfab67909cac68f47419a8f15f63bf
Ciro Balzano <cirobalzano93@gmail.com>
2023-04-27 15:47:51 +0200

    Counter

--- assets/js/script-responsive.js
+++ assets/js/script-responsive.js
@@ -8,6 +8,18 @@ function onMediaQueryChange(mq) {
     ermione.style.width = "30%";
     harry.style.width = "30%";
     voldemort.style.width = "30%"
+    harry.style.position = "absolute"
+    harry.style.bottom = "0"
+    harry.style.right = "0"
+    ermione.style.position = "absolute"
+    ermione.style.top = "0"
+    ermione.style.left = "0"
+    scopaMinus.style.position = "absolute"
+    scopaMinus.style.top = "0"
+    scopaMinus.style.left = "0"
+    scopaPlus.style.position = "absolute"
+    scopaPlus.style.bottom = "0"
+    scopaPlus.style.right = "0"
   } else {
     // la media query non è attiva
     scopaMinus.style.width = "5%";
@@ -19,7 +31,7 @@ function onMediaQueryChange(mq) {
 }
 
 // registra la funzione di callback per la media query
-let mq = window.matchMedia("(max-width: 960px)");
+let mq = window.matchMedia("(max-width: 1280px)");
 mq.addListener(onMediaQueryChange);
 
 // esegui la funzione di callback all'avvio della pagina
